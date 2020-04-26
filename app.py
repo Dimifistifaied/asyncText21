66359cd818b50081243bf888edee4cf289dfa7ce
dpopov1990 <dimiturpopov1990@gmail.com>
2020-04-26 16:16:25 +0300

    initial

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, abort, jsonify
+from Infrastructure import Runnable as rn
+
+app = Flask(__name__, static_folder='', static_url_path='')
+app.ls = []
+
+
+@app.route('/')
+def init():
+    return render_template('guz.html')
+
+
+@app.route('/post', methods=['POST'])
+def set_tasks():
+    if not request.json or 'text' not in request.json:
+        abort(400)
+    else:
+        app.ls = request.json['text']
+    return str(rn.createFile(app.ls))
+
+
+@app.route('/get', methods=['GET'])
+def get_task():
+    print(app.ls[:])
+    return app.ls[:]
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ js/API.js
@@ -0,0 +1,28 @@
+function sendToAPI(responseArea, text){
+         const data = {text: text}
+         responseArea = postData('/post', data)
+     }
+      async function postData(url = '', data = {}) {
+          // Default options are marked with *
+            const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              'Content-Type': 'application/json'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *client
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+          });
+           const text = await response.json()
+           document.getElementById('outputArea').value = text
+      }
+
+     async function updateTextArea() {
+          const response = await fetch('/get')
+          const text  = await response.text()
+          document.getElementById('textArea').value = text
+      }
