298bc6c01fdf831df8cdd45fd3a092b4a8b79629
dpopov1990 <dimiturpopov1990@gmail.com>
2020-04-26 16:59:58 +0300

    im

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, abort, jsonify
-#from Infrastructure import Runnable as rn
+from Infrastructure import Runnable as rn
 
 app = Flask(__name__, static_folder='', static_url_path='')
 app.ls = []
@@ -10,14 +10,14 @@ def init():
     return render_template('guz.html')
 
 
-"""@app.route('/post', methods=['POST'])
+@app.route('/post', methods=['POST'])
 def set_tasks():
     if not request.json or 'text' not in request.json:
         abort(400)
     else:
         app.ls = request.json['text']
     return str(rn.createFile(app.ls))
-"""
+
 
 @app.route('/get', methods=['GET'])
 def get_task():
